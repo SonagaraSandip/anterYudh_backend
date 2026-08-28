56e644b57f37fdb990310a3fb54d872e6e78710f
SonagaraSandip <104724019+SonagaraSandip@users.noreply.github.com>
2026-08-28 15:46:21 +0530

    small update

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ import { activeDbName } from './config/db.js';
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
